77d720f76d7be3b8e6fc764604352b0621cf5194
André Horman <andre.horman1994@hotmail.com>
2023-06-01 20:05:33 -0300

    Update 06.repeatedFilters.cy.js

--- cypress/e2e/06.repeatedFilters.cy.js
+++ cypress/e2e/06.repeatedFilters.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
-import mockFetch from '../mocks/fetch';
-import { COLUMN_FILTER } from '../utils/dataTestIds';
+import mockFetch from "../mocks/fetch";
+import { COLUMN_FILTER } from "../utils/dataTestIds";
 
 const FILTERED_ROWS_COUNT_POPULATION = 8;
 const FILTERED_ROWS_COUNT_DIAMETER = 4;
@@ -9,118 +9,175 @@ const FILTERED_ROWS_COUNT_ORBITAL_PERIOD = 8;
 const FILTERED_ROWS_COUNT_ROTATION_PERIOD = 3;
 const FILTERED_ROWS_COUNT_SURFACE_WATER = 3;
 
-describe('6 - Não utilize filtros repetidos', () => {
+describe("6 - Não utilize filtros repetidos", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/', {
+    cy.visit("http://localhost:3000/", {
       onBeforeLoad(win) {
-        cy.stub(win, 'fetch').callsFake(mockFetch);
+        cy.stub(win, "fetch").callsFake(mockFetch);
       },
     });
   });
 
-  it('Filtre por população e o remove das opções', () => {
-    const allColumnsOptions = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  it("Filtre por população e o remove das opções", () => {
+    const allColumnsOptions = [
+      "population",
+      "orbital_period",
+      "diameter",
+      "rotation_period",
+      "surface_water",
+    ];
 
-    cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-      expect(options).to.have.length(allColumnsOptions.length);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length);
 
-      allColumnsOptions.forEach((option) => {
-        expect(options).to.contain(option);
+        allColumnsOptions.forEach((option) => {
+          expect(options).to.contain(option);
+        });
       });
-    });
 
-    cy.addFilter('population', 'maior que', '8000');
+    cy.addFilter("population", "maior que", "8000");
 
-    cy.get('table tr').should('have.length', FILTERED_ROWS_COUNT_POPULATION);
+    cy.get("table tr").should("have.length", FILTERED_ROWS_COUNT_POPULATION);
 
-    cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-      expect(options).to.have.length(allColumnsOptions.length - 1);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length - 1);
 
-      expect(options).to.not.contain('population');
-    });
-  });
-});
-it('Filtre por diâmetro e o remove das opções', () => {
-  const allColumnsOptions = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length);
-
-    allColumnsOptions.forEach((option) => {
-      expect(options).to.contain(option);
-    });
+        expect(options).to.not.contain("population");
+      });
   });
 
-  cy.addFilter('diameter', 'maior que', '12240');
+  it("Filtre por diâmetro e o remove das opções", () => {
+    const allColumnsOptions = [
+      "population",
+      "orbital_period",
+      "diameter",
+      "rotation_period",
+      "surface_water",
+    ];
 
-  cy.get('table tr').should('have.length', FILTERED_ROWS_COUNT_DIAMETER);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length);
 
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length - 1);
+        allColumnsOptions.forEach((option) => {
+          expect(options).to.contain(option);
+        });
+      });
 
-    expect(options).to.not.contain('diameter');
+    cy.addFilter("diameter", "maior que", "12240");
+
+    cy.get("table tr").should("have.length", FILTERED_ROWS_COUNT_DIAMETER);
+
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length - 1);
+
+        expect(options).to.not.contain("diameter");
+      });
   });
-});
-it('Filtre por período de rotação e o remove das opções', () => {
-  const allColumnsOptions = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  it("Filtre por período de rotação e o remove das opções", () => {
+    const allColumnsOptions = [
+      "population",
+      "orbital_period",
+      "diameter",
+      "rotation_period",
+      "surface_water",
+    ];
 
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length);
 
-    allColumnsOptions.forEach((option) => {
-      expect(options).to.contain(option);
-    });
+        allColumnsOptions.forEach((option) => {
+          expect(options).to.contain(option);
+        });
+      });
+
+    cy.addFilter("rotation_period", "maior que", "25");
+
+    cy.get("table tr").should(
+      "have.length",
+      FILTERED_ROWS_COUNT_ROTATION_PERIOD
+    );
+
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length - 1);
+
+        expect(options).to.not.contain("rotation_period");
+      });
   });
+  it("Filtre por período de órbita e o remove das opções", () => {
+    const allColumnsOptions = [
+      "population",
+      "orbital_period",
+      "diameter",
+      "rotation_period",
+      "surface_water",
+    ];
 
-  cy.addFilter('rotation_period', 'maior que', '25');
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length);
 
-  cy.get('table tr').should('have.length', FILTERED_ROWS_COUNT_ROTATION_PERIOD);
+        allColumnsOptions.forEach((option) => {
+          expect(options).to.contain(option);
+        });
+      });
 
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length - 1);
+    cy.addFilter("orbital_period", "maior que", "350");
 
-    expect(options).to.not.contain('rotation_period');
+    cy.get("table tr").should(
+      "have.length",
+      FILTERED_ROWS_COUNT_ORBITAL_PERIOD
+    );
+
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length - 1);
+
+        expect(options).to.not.contain("orbital_period");
+      });
   });
-});
-it('Filtre por período de órbita e o remove das opções', () => {
-  const allColumnsOptions = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  it("Filtre por superfície aquática e o remove das opções", () => {
+    const allColumnsOptions = [
+      "population",
+      "orbital_period",
+      "diameter",
+      "rotation_period",
+      "surface_water",
+    ];
 
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length);
 
-    allColumnsOptions.forEach((option) => {
-      expect(options).to.contain(option);
-    });
-  });
+        allColumnsOptions.forEach((option) => {
+          expect(options).to.contain(option);
+        });
+      });
 
-  cy.addFilter('orbital_period', 'maior que', '350');
+    cy.addFilter("surface_water", "maior que", "99");
 
-  cy.get('table tr').should('have.length', FILTERED_ROWS_COUNT_ORBITAL_PERIOD);
+    cy.get("table tr").should("have.length", FILTERED_ROWS_COUNT_SURFACE_WATER);
 
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length - 1);
+    cy.getByTestId(COLUMN_FILTER)
+      .find("option")
+      .should((options) => {
+        expect(options).to.have.length(allColumnsOptions.length - 1);
 
-    expect(options).to.not.contain('orbital_period');
-  });
-});
-it('Filtre por superfície aquática e o remove das opções', () => {
-  const allColumnsOptions = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length);
-
-    allColumnsOptions.forEach((option) => {
-      expect(options).to.contain(option);
-    });
-  });
-
-  cy.addFilter('surface_water', 'maior que', '99');
-
-  cy.get('table tr').should('have.length', FILTERED_ROWS_COUNT_SURFACE_WATER);
-
-  cy.getByTestId(COLUMN_FILTER).find('option').should((options) => {
-    expect(options).to.have.length(allColumnsOptions.length - 1);
-
-    expect(options).to.not.contain('surface_water');
+        expect(options).to.not.contain("surface_water");
+      });
   });
 });
