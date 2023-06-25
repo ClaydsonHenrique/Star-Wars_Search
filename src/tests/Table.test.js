fb58b71f0bc207e44dd2873962499a4da3a66c56
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-06-25 19:39:37 -0300

    iniciando requisito 8

--- src/tests/Table.test.js
+++ src/tests/Table.test.js
@@ -1,68 +1,41 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { act } from "react-dom/test-utils";
 import App from '../App';
+import data from './helpers/data';
+
+
 
 
 describe('Verificando página Table', () => {
+
   it('deve fazer a requisição à API e renderizar os componentes corretamente', async () => {
-
-
-    const mockApiResponse = {
-      results: [
-        {
-          name: 'Tatooine',
-          rotation_period: '23',
-          orbital_period: '304',
-          diameter: '10465',
-          climate: 'arid',
-          gravity: '1 standard',
-          terrain: 'desert',
-          surface_water: '1',
-          population: '200000',
-          residents: [
-            'https://swapi-trybe.herokuapp.com/api/people/1/',
-            'https://swapi-trybe.herokuapp.com/api/people/2/',
-            'https://swapi-trybe.herokuapp.com/api/people/4/',
-            'https://swapi-trybe.herokuapp.com/api/people/6/',
-            'https://swapi-trybe.herokuapp.com/api/people/7/',
-            'https://swapi-trybe.herokuapp.com/api/people/8/',
-            'https://swapi-trybe.herokuapp.com/api/people/9/',
-            'https://swapi-trybe.herokuapp.com/api/people/11/',
-            'https://swapi-trybe.herokuapp.com/api/people/43/',
-            'https://swapi-trybe.herokuapp.com/api/people/62/',
-          ],
-          films: [
-            'https://swapi-trybe.herokuapp.com/api/films/1/',
-            'https://swapi-trybe.herokuapp.com/api/films/3/',
-            'https://swapi-trybe.herokuapp.com/api/films/4/',
-            'https://swapi-trybe.herokuapp.com/api/films/5/',
-            'https://swapi-trybe.herokuapp.com/api/films/6/',
-          ],
-          created: '2014-12-09T13:50:49.641000Z',
-          edited: '2014-12-20T20:58:18.411000Z',
-          url: 'https://swapi-trybe.herokuapp.com/api/planets/1/',
-        },
-      ],
-    };
 
     jest.spyOn(global, 'fetch');
     global.fetch.mockResolvedValue({
-      json: jest.fn().mockResolvedValue(mockApiResponse),
+      json: jest.fn().mockResolvedValue(data),
     });
-
 
     await act(async () => {
       render(<App></App>);
     });
 
 
-      const a = screen.getByTestId('button-remove-filters')
-      expect(a).toBeInTheDocument()
-      const b = screen.getByRole('cell', {
-        name: /tadtooine/i
-      })
-      expect(b).toBeInTheDocument()
+    const a = screen.getByTestId('button-remove-filters')
+    const c = screen.getByTestId('name-filter')
+    expect(a).toBeInTheDocument()
+    expect(c).toBeInTheDocument()
+    const b = screen.getByRole('columnheader', {
+      name: /name/i
+    })
+    const thElements = screen.getAllByRole('columnheader');
+    expect(thElements).toHaveLength(13);
+    const rowElements = screen.getAllByRole('row');
+    expect(rowElements).toHaveLength(11);
+    expect(b).toBeInTheDocument()
 
+    fireEvent.change(c, { target: { value: 'Tatooine' } })
   });
+
+
 });
